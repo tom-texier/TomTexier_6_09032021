603f1906cdd5bde34bab55c6049dea366a83685c
Tom Texier <tom.texierpro@gmail.com>
2021-03-20 19:44:57 +0100

    Get one by ID + Update

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,7 +2,6 @@ const Sauce = require('../models/Sauce');
 
 exports.createSauce = (req, res, next) => {
     const sauceObjet = JSON.parse(req.body.sauce);
-    console.log(sauceObjet);
     const sauce = new Sauce({
         ...sauceObjet,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -13,5 +12,30 @@ exports.createSauce = (req, res, next) => {
 }
 
 exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then((sauces) => { res.status(200).json(sauces); })
+        .catch((error) => { res.status(400).json({ error }); });
+}
 
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+            _id: req.params.id
+        })
+        .then((sauce) => { res.status(200).json(sauce) })
+        .catch((error) => { res.status(404).json({ error }) });
+}
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body }
+    Sauce.updateOne({
+            _id: req.params.id
+        }, {
+            ...sauceObject,
+            _id: req.params.id
+        })
+        .then((sauce) => { res.status(200).json(sauce) })
+        .catch((error) => { res.status(400).json({ error }) })
 }
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,5 +8,7 @@ const sauceCtlr = require('../controllers/sauce');
 
 router.post('/', auth, multer, sauceCtlr.createSauce);
 router.get('/', auth, sauceCtlr.getAllSauces);
+router.get('/:id', auth, sauceCtlr.getOneSauce);
+router.put('/:id', auth, multer, sauceCtlr.modifySauce);
 
 module.exports = router;
