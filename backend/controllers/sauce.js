22f954551b831e2281471529b5c963f1baeed0d8
Tom Texier <tom.texierpro@gmail.com>
2021-03-29 15:00:37 +0200

    Ajout d'un argument _id aux fonctions Moogoose pour être sûr de l'ID

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -32,14 +32,23 @@ exports.modifySauce = (req, res, next) => {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : {...req.body }
-    Sauce.updateOne({
-            _id: req.params.id
-        }, {
-            ...sauceObject,
-            _id: req.params.id
-        })
-        .then(sauce => res.status(200).json({ message: 'La sauce a bien été modifiée !' }))
-        .catch(error => res.status(400).json({ error }))
+    if (req.file) {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+            Sauce.deleteOne({ _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'La sauce a bien été suprimmée !' }))
+                .catch(error => res.status(400).json({ error }));
+        });
+    } else {
+        Sauce.updateOne({
+                _id: req.params.id
+            }, {
+                ...sauceObject,
+                _id: req.params.id
+            })
+            .then(sauce => res.status(200).json({ message: 'La sauce a bien été modifiée !' }))
+            .catch(error => res.status(400).json({ error }))
+    }
 }
 
 exports.deleteSauce = (req, res, next) => {
@@ -63,7 +72,8 @@ exports.opinionSauce = (req, res, next) => {
                     if (sauce.usersLiked.find(user => user === req.body.userId)) {
                         Sauce.updateOne({ _id: req.params.id }, {
                                 $inc: { likes: -1 },
-                                $pull: { usersLiked: req.body.userId }
+                                $pull: { usersLiked: req.body.userId },
+                                _id: req.params.id
                             })
                             .then(() => res.status(201).json({ message: 'Ton avis a été pris en compte!' }))
                             .catch(error => res.status(400).json({ error }));
@@ -71,7 +81,8 @@ exports.opinionSauce = (req, res, next) => {
                     if (sauce.usersDisliked.find(user => user === req.body.userId)) {
                         Sauce.updateOne({ _id: req.params.id }, {
                                 $inc: { dislikes: -1 },
-                                $pull: { usersDisliked: req.body.userId }
+                                $pull: { usersDisliked: req.body.userId },
+                                _id: req.params.id
                             })
                             .then(() => res.status(201).json({ message: 'Ton avis a été pris en compte!' }))
                             .catch(error => res.status(400).json({ error }));
