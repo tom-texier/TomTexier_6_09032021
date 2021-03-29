554ef8845ed7cf482f5436a39cd33ecba063beb2
Tom Texier <tom.texierpro@gmail.com>
2021-03-29 17:49:01 +0200

    Required Message --> Models

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: {
+        type: String,
+        required: [true, 'L\'adresse mail est requise'],
+        unique: true
+    },
+    password: { type: String, required: [true, 'Le mot de passe est requis !'] }
 });
 
 userSchema.plugin(uniqueValidator);
--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
-    name: { type: String, required: true },
-    manufacturer: { type: String, required: true },
-    description: { type: String, required: true },
-    mainPepper: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    heat: { type: Number, required: true },
+    name: { type: String, required: [true, 'Le nom de la sauce est requis !'] },
+    manufacturer: { type: String, required: [true, 'Le fabricant de la sauce est requis !'] },
+    description: { type: String, required: [true, 'La description de la sauce est requise !'] },
+    mainPepper: { type: String, required: [true, 'Le principal ingrédient de la sauce est requis !'] },
+    imageUrl: { type: String, required: [true, 'L\'image de la sauce est requise !'] },
+    heat: { type: Number, required: [true, 'La puissance de la sauce est requise !'] },
     likes: { type: Number, required: false, default: 0 },
     dislikes: { type: Number, required: false, default: 0 },
     usersLiked: { type: [String], required: false },
